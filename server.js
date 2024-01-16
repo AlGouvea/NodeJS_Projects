9333624c582e367d5eba3c92bef448e2095a7597
AlGouvea <alvarohsgouvea@gmail.com>
2024-01-16 13:05:06 -0300

    Configuração inicial do Jeopardy

--- server.js
+++ server.js
@@ -4,9 +4,16 @@ const express = require('express');
 // Import path module for file paths
 const path = require('path');
 
+const socketIo = require('socket.io');
+
+const http = require('http');
+
 // Create express app and set port
 const app = express();
+const server = http.createServer(app);
 const port = 3000;
+
+const io = socketIo(server);
 
 //Serve stativ files from the public folder
 app.use(express.static(path.join(__dirname, 'public')));
@@ -16,7 +23,36 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-// Start server on given port
-app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+app.get('/jeopardy', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/games/Game2', 'jeopardy.html'));
+});
+
+const rooms = new Map();
+
+io.on('connection', (socket) => {
+    // Handle room creation
+    socket.on('createRoom', (roomCode) => {
+        if (!rooms.has(roomCode)) {
+            rooms.set(roomCode, new Set([socket.id]));
+            socket.join(roomCode);
+            io.to(socket.id).emit('roomCreated', roomCode);
+        } else {
+            io.to(socket.id).emit('roomExists', roomCode);
+        }
+    });
+
+    // Handle joining a room
+    socket.on('joinRoom', (roomCode) => {
+        if (rooms.has(roomCode)) {
+            rooms.get(roomCode).add(socket.id);
+            socket.join(roomCode);
+            io.to(socket.id).emit('roomJoined', roomCode);
+        } else {
+            io.to(socket.id).emit('roomNotFound', roomCode);
+        }
+    });
+});
+
+server.listen(3000, () => {
+    console.log('Server is running on http://localhost:3000');
 });
